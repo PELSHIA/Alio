44f121023437886f8beb100a769a31bc3ef5374c
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-04-01 21:43:29 +0900

    [EDIT] Set Navigation

--- Alio/presentation/src/main/java/com/project/alio/view/activity/MainActivity.kt
+++ Alio/presentation/src/main/java/com/project/alio/view/activity/MainActivity.kt
@@ -2,11 +2,29 @@ package com.project.alio.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.project.alio.R
+import com.project.alio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
