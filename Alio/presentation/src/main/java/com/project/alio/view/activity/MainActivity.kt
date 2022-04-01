59b8391ac49f24f38c8004017473480e83894a6c
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-04-01 20:49:58 +0900

    [EDIT] Setting MainActivity

--- Alio/presentation/src/main/java/com/project/alio/view/activity/MainActivity.kt
+++ Alio/presentation/src/main/java/com/project/alio/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.project.alio
+package com.project.alio.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.alio.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
