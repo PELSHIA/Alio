e5847ee4402a16a2a08fbff43a3234ffa2eb59d7
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-04-16 20:16:19 +0900

    [ADD] Setting AlarmRepositoryImpl

---
+++ Alio/data/src/main/java/com/example/data/repository/AlarmRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.example.data.repository
+
+import com.example.data.mapper.mapperToAlarmEntity
+import com.example.data.mapper.mapperToAlarmList
+import com.example.data.repository.local.AlarmLocalDataSource
+import com.example.domain.model.Alarm
+import com.example.domain.repository.AlarmRepository
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+
+class AlarmRepositoryImpl(private val alarmLocalSource: AlarmLocalDataSource): AlarmRepository {
+    override fun allAlarmList(): Single<List<Alarm>> {
+        return alarmLocalSource.allAlarmList().flatMap {
+            Single.just(mapperToAlarmList(it))
+        }
+    }
+
+    override fun insertAlarm(alarm: Alarm): Completable {
+        return alarmLocalSource.insertAlarm(mapperToAlarmEntity(alarm))
+    }
+
+    override fun deleteAlarm(alarm: Alarm): Completable {
+        return alarmLocalSource.deleteAlarm(mapperToAlarmEntity(alarm))
+    }
+
+    override fun updateAlarm(alarm: Alarm): Completable {
+        return alarmLocalSource.updateAlarm(mapperToAlarmEntity(alarm))
+    }
+}
