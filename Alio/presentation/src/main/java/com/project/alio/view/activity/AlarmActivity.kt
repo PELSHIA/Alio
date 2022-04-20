d7e35e946e3f24e431fabaa263bea15c7b9c6d54
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-04-20 20:33:36 +0900

    [ADD] AlarmActivity

---
+++ Alio/presentation/src/main/java/com/project/alio/view/activity/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.project.alio.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.alio.R
+
+class AlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
