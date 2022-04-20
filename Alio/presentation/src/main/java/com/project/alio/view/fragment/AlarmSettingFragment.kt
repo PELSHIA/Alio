719e9716009a50a8bc505a4fb12d61e505f14732
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-04-20 20:34:09 +0900

    [ADD] Setting Alarm

--- Alio/presentation/src/main/java/com/project/alio/view/fragment/AlarmSettingFragment.kt
+++ Alio/presentation/src/main/java/com/project/alio/view/fragment/AlarmSettingFragment.kt
@@ -1,21 +1,40 @@
 package com.project.alio.view.fragment
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.data.db.sharedpreferences.RingtonePreferences
+import com.project.alio.util.receiver.AlarmBroadcastReceiver
+import com.example.domain.model.Alarm
 import com.project.alio.R
 import com.project.alio.databinding.FragmentAlarmSettingBinding
 import com.project.alio.view.activity.AlarmSettingActivity
+import com.project.alio.viewModel.AlarmViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class AlarmSettingFragment : Fragment() {
 
     private lateinit var binding: FragmentAlarmSettingBinding
+    private val viewModel: AlarmViewModel by viewModels()
+    private val calendar: Calendar = Calendar.getInstance()
+    private lateinit var alarmManager: AlarmManager
+    private val isSelect = ContextCompat.getDrawable(
+        activity as Context,
+        R.drawable.background_week_selected
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,8 +73,16 @@ class AlarmSettingFragment : Fragment() {
         val initCategory = resources.getStringArray(R.array.spinner_category)
         val initMission = resources.getStringArray(R.array.spinner_mission)
 
-        val categoryAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, initCategory)
-        val missionAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, initMission)
+        val categoryAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            initCategory
+        )
+        val missionAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            initMission
+        )
 
         binding.categorySpinner.adapter = categoryAdapter
         binding.missionSpinner.adapter = missionAdapter
@@ -63,7 +90,7 @@ class AlarmSettingFragment : Fragment() {
 
     private fun bindSpinner() {
         binding.categorySpinner.setOnItemClickListener { _, _, position, _ ->
-            when(position) {
+            when (position) {
                 1 -> {
                     TODO("카테고리 추가 다이얼로그 띄우기")
                 }
@@ -75,5 +102,74 @@ class AlarmSettingFragment : Fragment() {
         binding.ringtone.setOnClickListener {
             findNavController().navigate(R.id.action_alarmSettingFragment_to_ringtoneFragment)
         }
+    }
+
+    private fun bindCheckButton() {
+        binding.alarmSettingCheck.setOnClickListener {
+            setAlarmData()
+        }
+    }
+
+    private fun setAlarmData() {
+        setCalendar()
+        val alarmName = binding.alarmSettingName.text.toString()
+        val category = binding.categorySpinner.selectedItem.toString()
+        val mission = binding.missionSpinner.selectedItem.toString()
+        val ringtone = RingtonePreferences.ringtone
+
+        viewModel.insertAlarm(
+            Alarm(
+                UUID.randomUUID().toString(),
+                alarmName,
+                calendar,
+                category,
+                mission,
+                ringtone!!
+            )
+        )
+        settingAlarm()
+    }
+
+    private fun setCalendar() {
+        calendar.apply {
+            set(Calendar.HOUR_OF_DAY, binding.alarmHourPicker.value)
+            set(Calendar.MINUTE, binding.alarmMinutePicker.value)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+    }
+
+    private fun settingAlarm() {
+        val pIntent: PendingIntent = settingIntent()
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pIntent
+        )
+    }
+
+    private fun settingIntent(): PendingIntent {
+        val dayOfWeek: ArrayList<Boolean> = getDayOfWeek()
+        val intent: Intent = Intent(activity, AlarmBroadcastReceiver::class.java)
+        intent.putExtra("dayOfWeek", dayOfWeek)
+        return PendingIntent.getBroadcast(activity, 0, intent, 0)
+    }
+
+    private fun getDayOfWeek(): ArrayList<Boolean> {
+        return arrayListOf(
+            binding.monday.background == isSelect,
+            binding.tuesday.background == isSelect,
+            binding.wednesday.background == isSelect,
+            binding.Thursday.background == isSelect,
+            binding.friday.background == isSelect,
+            binding.saturday.background == isSelect,
+            binding.sunday.background == isSelect
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        RingtonePreferences.clear()
     }
 }
