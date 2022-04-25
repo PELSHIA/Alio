3450e74feb86315dccc401a1f23f6269ac4779f4
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-04-25 20:50:57 +0900

    [EDIT] SetTime

--- Alio/presentation/src/main/java/com/project/alio/util/binding/BindingAdapter.kt
+++ Alio/presentation/src/main/java/com/project/alio/util/binding/BindingAdapter.kt
@@ -1,11 +1,13 @@
 package com.project.alio.util.binding
 
 import android.content.Context
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.project.alio.R
+import java.util.*
 
 object BindingAdapter {
 
@@ -30,4 +32,23 @@ object BindingAdapter {
             }
         }
     }
+
+    @JvmStatic
+    @BindingAdapter("setTime")
+    fun bindSetTime(view: TextView, time: Calendar) {
+        val hour = time.get(Calendar.HOUR_OF_DAY)
+        val min = time.get(Calendar.MINUTE)
+        view.text = String.format("%02d:%02d", hour, min)
+    }
+
+    @JvmStatic
+    @BindingAdapter("setMainDayOfWeek")
+    fun bindSetMainDayOfWeek(view: TextView, isEnable: Boolean) {
+        if (isEnable)
+            view.setTextColor(ContextCompat.getColor(view.context, R.color.black))
+        else
+            view.setTextColor(ContextCompat.getColor(view.context, R.color.gray))
+
+    }
+
 }
