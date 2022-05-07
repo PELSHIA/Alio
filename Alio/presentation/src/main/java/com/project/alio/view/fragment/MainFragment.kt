f2882c3e4bf7b5984e5151f34e0fed0891cfaa0e
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-05-07 23:47:02 +0900

    [EDIT] Update & Delete Alarm

--- Alio/presentation/src/main/java/com/project/alio/view/fragment/MainFragment.kt
+++ Alio/presentation/src/main/java/com/project/alio/view/fragment/MainFragment.kt
@@ -1,5 +1,9 @@
 package com.project.alio.view.fragment
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,9 +11,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.domain.model.Alarm
 import com.project.alio.R
 import com.project.alio.databinding.FragmentMainBinding
 import com.project.alio.util.decorator.RecyclerViewDecoration
+import com.project.alio.util.receiver.AlarmBroadcastReceiver
+import com.project.alio.view.activity.AlarmSettingActivity
 import com.project.alio.view.adapter.AlarmRecyclerViewAdapter
 import com.project.alio.viewModel.AlarmViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -70,10 +77,10 @@ class MainFragment : Fragment() {
             val bottomSheet: AlarmBottomSheetFragment = AlarmBottomSheetFragment { state ->
                 when (state) {
                     0 -> {
-
+                        updateAlarm(true, it)
                     }
                     1 -> {
-
+                        updateAlarm(false, it)
                     }
                 }
             }
@@ -81,4 +88,24 @@ class MainFragment : Fragment() {
         }
     }
 
+    private fun updateAlarm(state: Boolean, alarm: Alarm) { // Alarm Update of Delete
+        when (state) {
+            true -> { // Update
+                val intent = Intent(activity, AlarmSettingActivity::class.java)
+                intent.putExtra("id", alarm.id)
+                intent.putExtra("state", true)
+                startActivity(intent)
+            }
+            false -> { // Delete
+                val dayOfWeek: ArrayList<Boolean> = arrayListOf()
+                val intent = Intent(activity, AlarmBroadcastReceiver::class.java)
+                val pendingIntent = PendingIntent.getBroadcast(activity, alarm.id, intent, 0)
+                val alarmManager = activity?.applicationContext?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                dayOfWeek.addAll(alarm.dayOfWeek)
+                intent.putExtra("dayOfWeek", dayOfWeek)
+                alarmManager.cancel(pendingIntent)
+                viewModel.deleteAlarm(alarm) // Local DB Delete
+            }
+        }
+    }
 }
