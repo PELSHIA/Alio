37159cebc583b809fbbb9d050db9ff8f9de07998
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-06-15 20:01:04 +0900

    [ADD] TodoList Mission Setting

--- Alio/presentation/src/main/java/com/project/alio/view/fragment/MissionTodoListFragment.kt
+++ Alio/presentation/src/main/java/com/project/alio/view/fragment/MissionTodoListFragment.kt
@@ -5,16 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.project.alio.R
+import com.project.alio.databinding.FragmentMissionTodoListBinding
 
 class MissionTodoListFragment : Fragment() {
+
+    private lateinit var binding: FragmentMissionTodoListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mission_todo_list, container, false)
+        binding = FragmentMissionTodoListBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.alarmTodoCheck.setOnClickListener {
+            todoEmpty()
+        }
+    }
+
+    private fun todoEmpty() {
+        if (binding.alarmTodo1.text.isEmpty() || binding.alarmTodo2.text.isEmpty() || binding.alarmTodo3.text.isEmpty()) {
+            Toast.makeText(context, "아직 할일을 모두 적지 않았어요.", Toast.LENGTH_SHORT).show()
+        } else {
+            activity?.finish()
+        }
+    }
 }
