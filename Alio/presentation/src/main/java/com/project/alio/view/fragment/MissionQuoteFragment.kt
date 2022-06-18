ae6eb7c6f8d5d943412ee268d0b841aa26d678f7
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-06-18 15:56:45 +0900

    [ADD] Mission Quote

--- Alio/presentation/src/main/java/com/project/alio/view/fragment/MissionQuoteFragment.kt
+++ Alio/presentation/src/main/java/com/project/alio/view/fragment/MissionQuoteFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.project.alio.R
+import com.project.alio.databinding.FragmentMissionQuoteBinding
+import java.util.*
 
 class MissionQuoteFragment : Fragment() {
+
+    private lateinit var binding: FragmentMissionQuoteBinding
+    private val quoteList = resources.getStringArray(R.array.array_quote)
+    private val quoteNameList = resources.getStringArray(R.array.array_quote_name)
+    private val random = Random().nextInt(quoteList.size)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mission_quote, container, false)
+        binding = FragmentMissionQuoteBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        settingQuote()
+        comparisonQuote()
+    }
+
+    private fun settingQuote() {
+        binding.alarmQuoteText.text = quoteList[random]
+        binding.alarmQuoteEditText.hint = quoteList[random]
+        binding.alarmQuotePeople.text = quoteNameList[random]
+    }
+
+    private fun comparisonQuote() {
+        binding.alarmQuoteCheck.setOnClickListener {
+            val writeQuote = binding.alarmQuoteEditText.text.toString()
+            if (writeQuote == quoteList[random]) {
+                activity?.finish()
+            } else {
+                Toast.makeText(activity, "다시 작성해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
