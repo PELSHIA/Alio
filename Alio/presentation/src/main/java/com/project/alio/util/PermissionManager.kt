ee1284a93f4834c39e2b4a86f69db2719c428d2c
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2022-06-20 11:29:59 +0900

    [ADD] Permission Check

---
+++ Alio/presentation/src/main/java/com/project/alio/util/PermissionManager.kt
@@ -0,0 +1,54 @@
+package com.project.alio.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionManager(val context: Context, val activity: Activity) {
+
+    private val MULTIPLE_PERMISSIONS = 1000
+    private lateinit var permissionList: ArrayList<String>
+    private val permissions: ArrayList<String> = arrayListOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+
+    // 거부가 된 권한이 있는지 확인
+    fun checkPermission(): Boolean {
+        var result: Int
+        permissionList = arrayListOf()
+
+        for (pm in permissions) {
+            result = ContextCompat.checkSelfPermission(context, pm)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(pm)
+            }
+        }
+        if (permissionList.isNotEmpty()) {
+            return false
+        }
+        return true
+    }
+
+    // 권한 요청
+    fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            activity, (permissionList.toTypedArray<String?>()), MULTIPLE_PERMISSIONS
+        )
+    }
+
+    // 권한 결과갑 판단 및 처리
+    fun permissionResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray): Boolean {
+        if (requestCode == MULTIPLE_PERMISSIONS && (grantResults.isNotEmpty())) {
+            for (result in grantResults) {
+                if (result == -1){ // 사용 거부
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
--- Alio/presentation/src/main/java/com/project/alio/view/activity/MainActivity.kt
+++ Alio/presentation/src/main/java/com/project/alio/view/activity/MainActivity.kt
@@ -4,23 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.project.alio.R
 import com.project.alio.databinding.ActivityMainBinding
+import com.project.alio.util.PermissionManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var permission: PermissionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        getPermission()
         initNavigation()
         navigateAlarmSetting()
     }
@@ -34,9 +40,28 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun getPermission() {
+        permission = PermissionManager(this, this)
+        if (!permission.checkPermission()) {
+            permission.requestPermission()
+        }
+    }
+
     private fun navigateAlarmSetting() {
         binding.fab.setOnClickListener {
             startActivity(Intent(this, AlarmSettingActivity::class.java))
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (!permission.permissionResult(requestCode, permissions, grantResults)) {
+            permission.requestPermission()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
     }
 }
